213347c442b79671d359f3312eb33e797b7f7e5d
Maxtreme69 <s3853476@student.rmit.edu.au>
2024-05-11 07:06:23 +1000

    added Footer columns
    added two columns to Footer component

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import React from 'react';
 
 function Footer() {
   return (
     <div className="footer">
+      <div className="footer-column">
+        <h3>Account</h3>
+        <ul>
+          <li>My Tests</li>
+          <li>Vote</li>
+          <li>Settings</li>
+          <li>Logout</li>
+        </ul>
+      </div>
+      <div className="footer-column">
+        <h3>Resources</h3>
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>FAQ</li>
+          <li>Need Help?</li>
+          <li>Give Feedback</li>
+          <li>Report a Bug</li>
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
