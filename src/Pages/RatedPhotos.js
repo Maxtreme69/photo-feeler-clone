840f66b8148b6729c5622de07cd583bae21a2bef
Maxtreme69 <s3853476@student.rmit.edu.au>
2024-05-22 15:39:07 +1000

    sort rated photos by category
    updated RatedPhoto component to sort rated photos by selectedCategory passed in submissionDataList object.

--- src/Pages/RatedPhotos.js
+++ src/Pages/RatedPhotos.js
@@ -1,5 +1,4 @@
-// RatedPhotos.js
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import ImageCardComponent from '../Components/ImageCardComponent';
 import CustomDropdown from '../Components/CustomDropdown';
 import { SubmissionDataContext } from '../Context/SubmissionDataContext';
@@ -9,53 +8,49 @@ const RatedPhotos = () => {
   const [selectedCategory, setSelectedCategory] = useState('dating');
   const { submissionDataList, setSubmissionDataList } = useContext(SubmissionDataContext);
 
-  const handleSubmissionData = (data) => {
-    setSubmissionDataList((prevList) => [...prevList, data]);
-  };
+  useEffect(() => {
+    const selectedData = submissionDataList.find(data => data.selectedCategory === selectedCategory.toLowerCase());
+    if (selectedData) {
+      setSelectedCategory(selectedData.selectedCategory);
+    }
+  }, [selectedCategory, submissionDataList]);
 
-  const addTestData = () => {
-    const newTestData = {
-      selectedOption: 'sample_image.jpg',
-      selectedCategory: 'dating',
-      selections: {
-        smart: 1,
-        trustworthy: 2,
-        attractive: 3,
-      },
-    };
-    setSubmissionDataList((prevList) => [...prevList, newTestData]);
+  const handleCategorySelect = (selectedOption) => {
+    setSelectedCategory(selectedOption.toLowerCase());
   };
 
   return (
     <div>
-      {/* <div style={{ width: '267px', padding: '20px 0 0 20px' }}>
+      <div style={{ width: '267px', padding: '20px 0 0 20px' }}>
         <CustomDropdown
           options={['DATING', 'SOCIAL', 'BUSINESS']}
           selectedOption={selectedCategory.toUpperCase()}
           onOptionSelect={handleCategorySelect}
         />
-      </div> */}
+      </div>
       {submissionDataList.length > 0 && (
         <div className="image-cards" style={{ marginTop: '20px' }}>
-          {submissionDataList.map((data, index) => (
-            <ImageCardComponent
-              key={`${data.selectedOption}-${data.selectedCategory}`}
-              image={data.selectedOption}
-              category={data.selectedCategory.toUpperCase()}
-              ratings={data.selections}
-              className="image-card"
-            />
-          ))}
+          {submissionDataList.map((data, index) => {
+            if (data.selectedCategory === selectedCategory.toLowerCase()) {
+              return (
+                <ImageCardComponent
+                  key={`${data.selectedOption}-${data.selectedCategory}`}
+                  image={data.selectedOption}
+                  category={data.selectedCategory.toUpperCase()}
+                  ratings={data.selections}
+                  className="image-card"
+                />
+              );
+            }
+            return null; // Skip rendering if not the selected category
+          })}
         </div>
       )}
-      <div>
+      {/* <div>
         <h3>Submission Data List:</h3>
         <pre>{JSON.stringify(submissionDataList, null, 2)}</pre>
         <button onClick={addTestData}>Add Test Submission</button>
-      </div>
-      <div>
-        
-      </div>
+      </div> */}
     </div>
   );
 };
