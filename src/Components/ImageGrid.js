8316ad739442419a1c1f163c9e6d4432f6f5a29e
Maxtreme69 <s3853476@student.rmit.edu.au>
2024-05-28 13:31:54 +1000

    conditionally render delete image buttons
    conditionally rendered delete image buttons so that different prompts appear for button based on whether uploaded image is displayed, image is checked or unchecked.

--- src/Components/ImageGrid.js
+++ src/Components/ImageGrid.js
@@ -5,6 +5,7 @@ import { RectanglesContext } from '../Context/RectanglesContext';
 function ImageGrid({ setSelectedImage, handleStepChange }) {
   const { rectangles, setRectangles } = useContext(RectanglesContext);
   const [checkedRectangles, setCheckedRectangles] = useState([]);
+  const [showCheckboxes, setShowCheckboxes] = useState(false);
 
   const addRectangle = (file) => {
     const reader = new FileReader();
@@ -22,9 +23,7 @@ function ImageGrid({ setSelectedImage, handleStepChange }) {
 
   const handleImageChange = (event) => {
     const file = event.target.files[0];
-    if (file) {
-      addRectangle(file);
-    }
+    addRectangle(file);
   };
 
   const getRandomColor = () => {
@@ -37,7 +36,6 @@ function ImageGrid({ setSelectedImage, handleStepChange }) {
   };
 
   const handleCheckboxChange = (id) => {
-    console.log(id); // Log the id when checkbox is clicked
     if (checkedRectangles.includes(id)) {
       setCheckedRectangles(checkedRectangles.filter(rectId => rectId !== id));
     } else {
@@ -53,16 +51,19 @@ function ImageGrid({ setSelectedImage, handleStepChange }) {
     setCheckedRectangles([]);
   };
 
+  const toggleCheckboxes = () => {
+    setShowCheckboxes(!showCheckboxes);
+  };
+
   return (
     <div className="image-grid">
       <div className="grid-container">
         <div style={{ marginTop: '10px' }} className="button-container">
           <label htmlFor="upload-input">
             <div className="upload-box">
+              <FaUpload style={{ fontSize: '24px', marginLeft: '35px' }} />
               <div className="upload-text">UPLOAD</div>
-              <div className="upload-icon">
-                <FaUpload />
-              </div>
+              <div className="upload-icon"></div>
               <input
                 id="upload-input"
                 type="file"
@@ -82,27 +83,50 @@ function ImageGrid({ setSelectedImage, handleStepChange }) {
             {rectangle.image && (
               <img src={rectangle.image} alt="Uploaded" className="rectangle-image" />
             )}
-            <input
-              type="checkbox"
-              className="rectangle-checkbox"
-              style={{ position: 'absolute', top: '5px', left: '5px' }}
-              checked={checkedRectangles.includes(rectangle.id)}
-              onChange={() => handleCheckboxChange(rectangle.id)}
-            />
+            {showCheckboxes && (
+              <input
+                type="checkbox"
+                className="rectangle-checkbox"
+                style={{ position: 'absolute', top: '5px', left: '5px' }}
+                checked={checkedRectangles.includes(rectangle.id)}
+                onChange={() => handleCheckboxChange(rectangle.id)}
+              />
+            )}
           </div>
         ))}
       </div>
 
-      <div className="button-container">
-        <label htmlFor="delete-button">
-          <div className="upload-box" onClick={deleteCheckedImage}>
-            <div className="upload-text">DELETE PHOTOS</div>
-            <div className="upload-icon">
-              <FaTrash />
+      {rectangles.length > 0 && !showCheckboxes && (
+        <div className="button-container">
+          <label htmlFor="toggle-checkboxes">
+            <div style={{ textAlign: 'center' }} className="upload-box" onClick={toggleCheckboxes}>
+              <FaTrash style={{ fontSize: '24px', marginLeft: '35px' }} />
+              <div className="upload-text">DELETE PHOTOS</div>
+              <div className="upload-icon"></div>
             </div>
-          </div>
-        </label>
-      </div>
+          </label>
+        </div>
+      )}
+
+      {rectangles.length > 0 && showCheckboxes && (
+        <div className="button-container">
+          {checkedRectangles.length > 0 ? (
+            <label htmlFor="delete-button">
+              <div className="upload-box" onClick={deleteCheckedImage}>
+                <div className="upload-text">DELETE SELECTED PHOTOS</div>
+                <div className="upload-icon">
+                  <button className="delete-selected-photos-button">Delete</button>
+                </div>
+              </div>
+            </label>
+          ) : (
+            <div className="upload-box" onClick={toggleCheckboxes}>
+              <div className="upload-text">SELECT PHOTOS TO DELETE</div>
+              <button className="delete-photos-button orange">Cancel</button>
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
 }
