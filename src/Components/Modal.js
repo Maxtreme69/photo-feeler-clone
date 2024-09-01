93636988596200f772ac158ae9cfabfcbadd23bd
Maxtreme69 <s3853476@student.rmit.edu.au>
2024-09-01 23:17:20 +1000

    modified dimensions of modal windo
    modified the dimensions of modal window in RatedPhotos to be more responsive across desktop and laptop devices

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -9,17 +9,17 @@ const Modal = ({ isVisible, onClose, image, ratings, category, textareaContent, 
   return (
     <div className="modal-overlay" onClick={onClose}>
       <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-          <div style={{ padding: '0 20px', overflow: 'hidden', display: 'flex', justifyContent: 'center', paddingTop: '20px' }}>
-            <img src={image} alt="Selected" style={{ width: '400px', height: '650px', objectFit: 'cover' }} />
+        <div style={{   padding: '20px 40px 20px 20px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          <div style={{ overflow: 'hidden', display: 'flex', justifyContent: 'center' }}>
+            <img src={image} alt="Selected" style={{ height: '650px', objectFit: 'cover' }} />
           </div>
           {selectedImageDetailsSubmit && (category === 'dating' || category === 'social') ? (
-            <div style={{ background: 'rgba(255, 255, 255, 0.8)', padding: '10px', borderRadius: '8px', width: '400px' }}>
+            <div style={{ background: 'rgba(255, 255, 255, 0.😎', padding: '10px', borderRadius: '8px', width: '400px' }}>
               <p className="image-details-dating">SUBJECT</p>
               <p className="image-details">{selectedImageDetailsSubmit}</p>
             </div>
           ) : selectedImageDetailsSubmit && category === 'business' ? (
-            <div style={{ background: 'rgba(255, 255, 255, 0.8)', padding: '10px', borderRadius: '8px', width: '400px' }}>
+            <div style={{ background: 'rgba(255, 255, 255, 0.😎', padding: '10px', borderRadius: '8px' }}>
               <p className="image-details-dating">TITLE</p>
               <p className="image-details">{selectedImageDetailsSubmit}</p>
             </div>
